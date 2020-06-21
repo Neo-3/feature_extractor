b0c4430f76c4d8fdc70388e369ff3ad5eff294da
OtacilioN <osmn@cesar.org.br>
2020-06-21 04:28:27 -0300

    Add feature extractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,11 +1,10 @@
 
 from keras.preprocessing import image
-from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
 
-def extract_image(img):
-    model = VGG16(weights='imagenet', include_top=False)
+def extract_image(img, model):
+    image.LOAD_TRUNCATED_IMAGES = True 
     img_data = image.img_to_array(img)
     img_data = np.expand_dims(img_data, axis=0)
     img_data = preprocess_input(img_data)
